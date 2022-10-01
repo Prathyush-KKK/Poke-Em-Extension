ab1b28cd9404fb6b681a7c7cefac42c85dff66e4
Prathyush K <74866178+Prathyush-KKK@users.noreply.github.com>
2022-10-01 23:27:57 +0530

    Updated UI

--- src/js/popup2.js
+++ src/js/popup2.js
@@ -1,5 +1,7 @@
 const urlArray = ["triller", "periscope", "vimeo", "valence", "untappd", "elpha", "yubo", "peanut", "houseparty", "caffeine", "steemit", "baidu tieba", "snaps", "likee", "tracks", "academia", "amikumu", "anobii", "asmallworld", "athlinks", "band", "bebee", "blind", "diaspora", "fark", "mewe", "facebook", "instagram", "twitter", "tumblr", "linkedin", "whatsapp", "snapchat", "pinterest", "reddit", "youtube", "mix", "tagged", "nextdoor", "deviantart", "quora", "meetup", "reverbnation", "flixster", "goodreads", "twitch", "caringbridge", "wattpad", "viadeo", "crunchyroll", "skyrock", "vk", "myheritage", "livejournal", "classmates", "soundcloud", "bubbly", "flickr", "we heart it", "influenster", "filmaffinity", "open diary", "yelp", "collegehumor", "gaia online", "mocospace", "couchsurfing", "funny or die", "italki", "etoro", "xing", "meetme", "ravelry", "care", "yy", "vero", "medium", "netflix", "prime video", "giphy", "giphy", "tribe", "tencent qq", "wechat", "qzone", "tiktok", "sina weibo", "kuaishou", "skype", "viber", "line", "line play", "the dots", "telegram", "foursquare swarm", "douban", "discord", "badoo", "myspace", "mixi", "ravelry", "cellufun", "xanga", "imgur", "ello", "wt social", "behance", "blogher", "dribbble", "letterboxd", "houzz", "stack overflow", "instructables", "cafemom", "dogster", "goodwall", "law link", "wayn", "supernatural connections", "octi", "clubhouse", "clouthub", "twitter spaces", "halloapp", "polywork", "poparazzi", "honk", "myanimelist", "barista exchange", "flip", "rumble", "stage ", "microsoft teams", "patreon", "spotify live", "substack"];
 var total = 0;
+var nameArr = new Array();
+var timeArr = new Array();
 
 function msToTime(ms) {
     var date = new Date(ms);
@@ -15,6 +17,8 @@ function getApps(topUrls) {    //display each app and used time
         var finUrl = rawUrl.charAt(0).toUpperCase() + rawUrl.slice(1);
         if(urlArray.indexOf(rawUrl)!== -1){
         total += url[1];
+        nameArr[i] = finUrl;
+        timeArr[i] = url[1];
     newDivs += "<div id=\"listWrapper\">" +  "<div id=\"nameApp\">" +  finUrl+ "</div>" + "<div id=\"timeUsed\" >" + msToTime(url[1]) + "</div>" + "</div>";
 }
     }
